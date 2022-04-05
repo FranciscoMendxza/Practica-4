6109871edc525c9c24a420453995320602e9e031
panch <panch@Mendxza>
2022-04-05 13:38:37 -0500

    PRACTICA 4

---
+++ src/Operaciones/Basicas.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Operaciones;
+
+/**
+ *
+ * @author panch
+ */
+public class Basicas {
+    
+    public int Sumar (int n1, int n2){
+        return (n1 + n2);
+    }
+
+    public int Restar (int n1, int n2){
+        return (n1 - n2);
+    }
+    
+    public int Multiplicar (int n1, int n2){
+        return (n1 * n2);
+    }
+    
+    public int Dividir (int n1, int n2){
+        return (n1 / n2);
+    }
+    
+}
